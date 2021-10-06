da4a78863eccf8552c03c1670294ba65b3dddbe0
Pervez Ali <contact.pervezali@gmail.com>
2021-10-06 16:51:27 +0600

    Home page posts and authros fetching completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <h1>Home Page</h1>
+    <div v-if="$fetchState.pending" class="mt-64 text-xl font-medium text-center">
+      <p>Loading...</p>
+    </div>
+    <div v-else-if="$fetchState.error" class="mt-64 text-xl font-medium text-center text-red-700">
+      <p>An error occurred :(</p>
+    </div>
+    <div v-else>
+      <PostsLists :posts="posts"/>
+      <AuthorsLists :authors="authors" section-title="Popular Authors"/>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  data: () => ({
+    posts: [],
+    authors: []
+  }),
 
+  async fetch() {
+    this.posts = await this.$axios.$get('blog/home/posts')
+    this.authors = await this.$axios.$get('blog/home/authors')
+  },
+}
 </script>
