f380de0cf80a3ec24eb59efebf1f463176f3ce60
Pervez Ali <contact.pervezali@gmail.com>
2021-10-06 16:53:12 +0600

    Authors index and show page dynamic data fetching completed

---
+++ pages/authors/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <AuthorsSingle :author="author" />
+    <PostsLists :posts="author.posts" :section-title="`Author Posts (${author.meta.posts_count})`" />
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({$axios, params, redirect}) {
+    try {
+      const author = await $axios.$get(`blog/authors/${params.id}`)
+      return { author }
+    } catch (error) {
+      redirect('/error')
+    }
+  },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/authors/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div v-if="$fetchState.pending" class="mt-64 text-xl font-medium text-center">
+    <p>Loading...</p>
+  </div>
+  <div v-else-if="$fetchState.error" class="mt-64 text-xl font-medium text-center text-red-700">
+    <p>An error occurred :(</p>
+  </div>
+  <AuthorsLists v-else :authors="authors"/>
+</div>
+
+</template>
+
+<script>
+export default {
+
+  data: () => ({
+    authors: []
+  }),
+
+  async fetch() {
+    this.authors = await this.$axios.$get('blog/authors')
+  },
+}
+</script>
