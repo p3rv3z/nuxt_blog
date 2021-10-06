443d6560ddd54797301bb8d2374dd7bc2426cc72
Pervez Ali <contact.pervezali@gmail.com>
2021-10-06 16:52:30 +0600

    Posts index and show page dynamic data fetching completed

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PostsSingle :post="post" />
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({$axios, params, redirect}) {
+    try {
+      const post = await $axios.$get(`blog/posts/${params.slug}`)
+      return { post }
+    } catch (error) {
+      redirect('/error')
+    }
+  },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div v-if="$fetchState.pending" class="mt-64 text-xl font-medium text-center">
+    <p>Loading...</p>
+  </div>
+  <div v-else-if="$fetchState.error" class="mt-64 text-xl font-medium text-center text-red-700">
+    <p>An error occurred :(</p>
+  </div>
+  <PostsLists v-else :posts="posts"/>
+</div>
+
+</template>
+
+<script>
+export default {
+
+  data: () => ({
+    posts: []
+  }),
+
+  async fetch() {
+    this.posts = await this.$axios.$get('blog/posts')
+  },
+}
+</script>
