fac5162cbcedd0a6cfd730248062aee55b206a50
Pervez Ali <contact.pervezali@gmail.com>
2021-10-06 16:53:39 +0600

    categories realted post fetching completed in categories show page

---
+++ pages/categories/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PostsLists :posts="category.posts" :section-title="`${category.name} (${category.meta.posts_count})`" />
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({$axios, params, redirect}) {
+      try {
+        const category = await $axios.$get(`blog/categories/${params.slug}`)
+        return { category }
+      } catch (error) {
+        redirect('/error')
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
